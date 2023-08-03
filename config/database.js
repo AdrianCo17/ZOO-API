28f4d81d0cf20e6acea1ddcac5be3cc11c173199
Adrian Cortes Estrada <123600209+AdrianCo17@users.noreply.github.com>
2023-08-03 15:55:49 -0500

    actualizacion

--- config/database.js
+++ config/database.js
@@ -1,8 +1,14 @@
 const { Sequelize } = require('sequelize');
+const configDB = './config';
 
-const sequelize = new Sequelize('zoo2', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
-
+const sequelize = new Sequelize(
+  "railway",
+  "root",
+  "ME7xIRWDUCpTGdzkmnN2",
+  {
+    host: "containers-us-west-121.railway.app",
+    port: 5652,
+    dialect: 'mysql',
+  }
+);
 module.exports = sequelize;
