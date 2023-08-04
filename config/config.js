99623c63c392b1d68e386a4fcdbcf03ae365c46f
AdrianCo17 <123600209+AdrianCo17@users.noreply.github.com>
2023-08-03 23:28:40 -0500

    config changes

--- config/config.js
+++ config/config.js
@@ -1,15 +1,12 @@
 const dotenv = require('dotenv');
-
 dotenv.config();
 
-const configDB = {
-  DB: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  dialect: 'mysql',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+const config = {
+  DB_NAME: process.env.DB_NAME,
+  DB_USER: process.env.DB_USER,
+  DB_PASS: process.env.DB_PASS,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
 };
 
-module.exports = configDB;
-
+module.exports = config;
