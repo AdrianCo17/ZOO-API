81de6851390df7213eb8d41db0bcf763bfac34d7
Adrian Cortes Estrada <123600209+AdrianCo17@users.noreply.github.com>
2023-08-03 23:44:44 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,12 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASS:', process.env.DB_PASS);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+
 const config = {
   DB_NAME: process.env.DB_NAME,
   DB_USER: process.env.DB_USER,
